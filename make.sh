31bef0ec69c0db95b30e07a855def7990b3980fb
almostdead3 <128373638+almostdead3@users.noreply.github.com>
2023-09-06 15:43:25 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 
 # Setup building environment
 
-sudo apt-get install git-core gnupg ccache lzop flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
+# sudo apt-get install git-core gnupg ccache lzop flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
 
 # make a working directory
 
@@ -12,7 +12,7 @@ cd port
 
 # Download firmware
 
-wget https://example.com/firmware.zip
+wget https://mirrors.lolinet.com/firmware/motorola/tundra_retcn/official/RETCN/XT2243-2_TUNDRA_RETCN_12_S3SJ32.1-79-10_subsidy-DEFAULT_regulatory-DEFAULT_CFC.xml.zip
 
 # Get a random zip file
 random_zip=$(ls *.zip | shuf -n 1)
